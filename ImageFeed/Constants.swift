044bb7c0eb58bca435062af11db6cba04c48f52f
Fennec-meow <krisbojchenko@gmail.com>
2025-01-20 15:30:00 +0300

    Constants.

---
+++ ImageFeed/Constants.swift
@@ -0,0 +1,17 @@
+//
+//  Constants.swift
+//  ImageFeed
+//
+//  Created by Kira on 20.01.2025.
+//
+
+import Foundation
+
+enum Constants {
+    static let accessKey = "F9m5lcPo6uJuYbyngJGb_7qqdOvgPuJN8jjDSnlV8RE"
+    static let secretKey = "aMsHV4IoKIOr-gFXW7ErR7TDJL4NEAAf20H6DLM9ACk"
+    static let redirectURI = "urn:ietf:wg:oauth:2.0:oob"
+    static let accessScope = "public+read_user+write_likes"
+    static let defaultBaseURL = URL(string: "https://api.unsplash.com")!
+}
+
